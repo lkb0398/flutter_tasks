714fedc6b155b9de2762c5b4b534f30f59bc8a85
kb <lkb0398@nate.com>
2025-10-18 22:57:40 +0900

    Update home_page.dart

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,56 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tasks/model/todo_model.dart';
 import 'package:flutter_tasks/view/todo_bottom_sheet.dart';
+import 'package:flutter_tasks/view/todo_view.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key, required this.title});
+  const HomePage({super.key, required this.title});
 
   final String title;
-  final List<TodoModel> list = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: Text(
           title,
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            width: double.infinity,
-            height: 220,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.grey,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/images/todo.webp"),
-                SizedBox(height: 10),
-                Text(
-                  "아직 할 일이 없습니다.",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  """할 일을 추가하고 $title에서
-                  할 일을 추척하세요.""",
-                  style: TextStyle(
-                    fontSize: 14,
-                    height: 1.5,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      body: TodoView(),
+      // NoTodoView(title: title),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
