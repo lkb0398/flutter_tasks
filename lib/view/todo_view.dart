9ded168eb888b0b0e0bf9e5a56119bd4f4ff278b
kb <lkb0398@nate.com>
2025-10-22 19:17:45 +0900

    Update todo_view.dart

--- lib/view/todo_view.dart
+++ lib/view/todo_view.dart
@@ -1,26 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tasks/model/todo_model.dart';
 
-class TodoView extends StatelessWidget {
-  const TodoView({
-    super.key,
-    required this.todo,
-    required this.onToggleFavorite,
-    required this.onToggleDone,
-  });
-
-  final TodoModel todo;
-  final VoidCallback onToggleFavorite;
-  final VoidCallback onToggleDone;
+class TodoView extends StatefulWidget {
+  const TodoView({super.key, required this.todoModel});
+  final TodoModel todoModel;
 
   @override
+  State<TodoView> createState() => _TodoViewState();
+}
+
+class _TodoViewState extends State<TodoView> {
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(Icons.circle, size: 10),
-        Text("title", style: TextStyle(fontSize: 30)),
-        Icon(Icons.star, size: 10),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Icon(Icons.arrow_back),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                widget.todoModel.isFavorite = !widget.todoModel.isFavorite;
+              });
+            },
+            child: widget.todoModel.isFavorite
+                ? Icon(Icons.star)
+                : Icon(Icons.star_border),
+          ),
+        ],
+      ),
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.todoModel.title,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Row(
+              spacing: 5,
+              children: [
+                Icon(Icons.short_text),
+                Text("세부 내용은 다음과 같습니다.", style: TextStyle(fontSize: 15)),
+              ],
+            ),
+            SizedBox(height: 10),
+            Text(
+              widget.todoModel.description.toString(),
+              style: TextStyle(fontSize: 15),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
